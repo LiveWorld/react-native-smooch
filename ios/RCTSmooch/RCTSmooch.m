1f751aa989de26023c25e736ab8025b01fa88981
Daniel Lam <dlam@liveworld.com>
2017-10-18 18:51:36 -0400

    update login & logout to return a promise

--- ios/RCTSmooch/RCTSmooch.m
+++ ios/RCTSmooch/RCTSmooch.m
@@ -13,22 +13,64 @@ RCT_EXPORT_METHOD(show) {
   });
 };
 
-RCT_EXPORT_METHOD(login:(NSString*)userId jwt:(NSString*)jwt completionHandler:(nullable void(^)(NSError * _Nullable error, NSDictionary * _Nullable userInfo))handler) {
+RCT_EXPORT_METHOD(login:(NSString*)userId jwt:(NSString*)jwt resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   NSLog(@"Smooch Login");
 
   dispatch_async(dispatch_get_main_queue(), ^{
-      [Smooch login:userId jwt:jwt completionHandler:handler];
+    [Smooch login:userId jwt:jwt completionHandler:^(NSError * _Nullable error, NSDictionary * _Nullable userInfo) {
+      if(error) {
+        reject(@"smooch_login_failed", @"Smooch login failed", error);
+      } else {
+        SKTUser *loggedInUser = userInfo[SKTUserIdentifier];
+        resolve([self _smoochUserToJSON:loggedInUser]);
+      }
+    }];
   });
 };
 
-RCT_EXPORT_METHOD(logoutWithCompletionHandler:(nullable void ( ^ ) ( NSError *_Nullable error , NSDictionary *_Nullable userInfo ))handler) {
+RCT_EXPORT_METHOD(logoutWithCompletionHandler:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   NSLog(@"Smooch Logout");
 
   dispatch_async(dispatch_get_main_queue(), ^{
-    [Smooch logoutWithCompletionHandler:handler];
+    [Smooch logoutWithCompletionHandler:^(NSError * _Nullable error, NSDictionary * _Nullable userInfo) {
+      if(error) {
+        reject(@"smooch_logout_failed", @"Smooch logout failed", error);
+      } else {
+        resolve(RCTJSONStringify(userInfo, NULL));
+      };
+    }];
   });
 };
 
+RCT_EXPORT_METHOD(currentUser:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  SKTUser *smoochUser = [SKTUser currentUser];
+  if(smoochUser) {
+    resolve([self _smoochUserToJSON:smoochUser]);
+  } else {
+    reject(@"current_user_failed", @"Unable to retrieve current_user", NULL);
+  }
+};
+
+- (NSString *)_smoochUserToJSON:(SKTUser *)user {
+  NSString *smoochUserJSON = @"{}";
+  NSDictionary *smoochUser = @{
+                               @"appUserId": [user appUserId] ? [user appUserId] : @"",
+                               @"userId": [user userId] ? [user userId] : @"",
+                               @"firstName": [user firstName] ? [user firstName] : @"",
+                               @"lastName": [user lastName] ? [user lastName] : @"",
+                               @"email": [user email] ? [user email] : @""
+                               };
+
+  NSError *error = nil;
+  if([NSJSONSerialization isValidJSONObject:smoochUser]) {
+    smoochUserJSON = RCTJSONStringify(smoochUser, &error);
+    if(error) {
+      NSLog(@"Error generating smoochUserJSON: %@", error);
+      return @"{}";
+    };
+  };
+  return smoochUserJSON;
+}
 
 RCT_EXPORT_METHOD(setUserProperties:(NSDictionary*)options) {
   NSLog(@"Smooch setUserProperties with %@", options);
